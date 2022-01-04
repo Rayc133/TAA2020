386f9bd330bd3e66d044d59e12fe134420f376bf
rayc133 <stefan.radic137@gmail.com>
2022-01-04 14:13:48 +0100

    FirstDayClass uppdated ty

--- src/com/taa2020/day1/FirstDayClass.java
+++ src/com/taa2020/day1/FirstDayClass.java
@@ -1,7 +1,9 @@
 package com.taa2020.day1;
 
-public class FirstDayClass {
+public class  FirstDayClass {
     public static void main(String[] args) {
 
+        int abc =123;
+        System.out.println(abc);
     }
 }
